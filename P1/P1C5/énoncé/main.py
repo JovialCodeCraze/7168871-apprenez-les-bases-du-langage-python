a616342de028800c7ea6563663ce20adf2e98ea6
Aimé_blikine <151871371+JovialCodeCraze@users.noreply.github.com>
2024-05-15 16:58:36 +0000

    Update main.py
    Dans cet exercice, j'explore les differents type de variable disponibles

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,9 @@
-# Écrivez votre code ici !
+nom = "Anne"
+age = 18
+taille = 1.79
+est_etudiant = True
+print(f"Je m'appelle {nom}, j'ai {age} ans et je mesure {taille} mètre.")
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)
